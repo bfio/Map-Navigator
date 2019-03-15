393f10bd514c891ff8b00aefca9b360050626c24
Dylan Richards <dylan.richards81@gmail.com>
2019-03-15 12:23:38 +0000

    Added Scene Builder to MainView

--- src/ca2/MainView.java
+++ src/ca2/MainView.java
@@ -1,16 +1,24 @@
 package ca2;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class MainView extends Stage{
 
-	public MainView() {
+	@FXML
+	private ImageView mapImageView;
+	
+	public MainView() throws IOException {
 		super.setTitle("Map Navigator");
 		
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 1200, 675);
+		Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+		Scene scene = new Scene(root);
 		
 		super.setScene(scene);
 		super.show();
