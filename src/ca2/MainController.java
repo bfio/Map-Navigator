3329ea13b8d185db357192ec557c5a94323519c5
Dylan Richards <dylan.richards81@gmail.com>
2019-03-20 19:28:19 +0000

    Updated view to display image of map.jpg

--- src/ca2/MainController.java
+++ src/ca2/MainController.java
@@ -2,8 +2,10 @@ package ca2;
 
 import ca2.MainModel;
 import ca2.MainView;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.image.Image;
 
 public class MainController implements EventHandler<ActionEvent>{
 
@@ -14,12 +16,21 @@ public class MainController implements EventHandler<ActionEvent>{
 		this.view = view;
 		this.model = model;
 
+		view.getAllMenuItems().forEach(m -> m.setOnAction(this));
+		
+		displayMap();
+	}
 
+	private void displayMap() {
+		Image img = new Image("file:map.jpg");
+		view.setImageView(img);
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		
+		if (event.getSource().equals(view.getMenuItemExit())) {
+			Platform.exit();
+		}
 	}
 	
 }
--- src/ca2/MainView.java
+++ src/ca2/MainView.java
@@ -1,27 +1,73 @@
 package ca2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainView extends Stage{
 
-	@FXML
-	private ImageView mapImageView;
+	private List<MenuItem> menuItems = new ArrayList<>();
+	private MenuItem menuItemExit;
+	
+	private Pane pane = new Pane();
+	private ImageView mapImageView = new ImageView();
 	
 	public MainView() throws IOException {
 		super.setTitle("Map Navigator");
 		
-		Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-		Scene scene = new Scene(root);
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 1250, 834);
+		
+		VBox menuBox = new VBox(getMenuBar());
+		root.setTop(menuBox);
+		
+		mapImageView.setPreserveRatio(false);
+		mapImageView.setFitHeight(833.5);
+		mapImageView.setFitWidth(1250);
+		pane.getChildren().add(mapImageView);
+		root.setCenter(pane);
 		
 		super.setScene(scene);
 		super.show();
+	}
+	
+	private MenuBar getMenuBar() {
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(createFileMenu());
+		return menuBar;
+	}
+	
+	private Menu createFileMenu() {
+		Menu menuFile = new Menu("File");
+
+		menuItemExit = new MenuItem("Exit");
+		menuItems.add(menuItemExit);
+
+		menuFile.getItems().addAll(menuItemExit);
+		return menuFile;
+	}
+
+	public List<MenuItem> getAllMenuItems() {
+		return menuItems;
+	}
+
+	public MenuItem getMenuItemExit() {
+		return menuItemExit;
+	}
+	
+	public void setImageView(Image img) {
+		mapImageView.setImage(img);
 	}
 	
 }
