878956a4503615bb7ebc3bfecd172bb539ff41a4
Dylan Richards <dylan.richards81@gmail.com>
2019-03-22 08:15:23 +0000

    Added ScrollPane to MainView so the image can be panned around

--- src/ca2/MainView.java
+++ src/ca2/MainView.java
@@ -8,10 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,22 +20,19 @@ public class MainView extends Stage{
 	private List<MenuItem> menuItems = new ArrayList<>();
 	private MenuItem menuItemExit;
 	
-	private Pane pane = new Pane();
+	private ScrollPane pane = new ScrollPane();
 	private ImageView mapImageView = new ImageView();
 	
 	public MainView() throws IOException {
 		super.setTitle("Map Navigator");
 		
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 1250, 834);
+		Scene scene = new Scene(root, 1280, 720);
 		
 		VBox menuBox = new VBox(getMenuBar());
 		root.setTop(menuBox);
 		
-		mapImageView.setPreserveRatio(false);
-		mapImageView.setFitHeight(833.5);
-		mapImageView.setFitWidth(1250);
-		pane.getChildren().add(mapImageView);
+		pane.setContent(mapImageView);
 		root.setCenter(pane);
 		
 		super.setScene(scene);
